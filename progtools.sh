4e791e39a56b80cbafb3dda1a1b5062adb80a990
Amr Alasmer <90987019+ammr01@users.noreply.github.com>
2023-12-29 21:20:09 +0300

    Update progtools.sh
    fix typo

--- progtools.sh
+++ progtools.sh
@@ -1945,7 +1945,7 @@ mkdir -p $programming_dir/{C,C++,python,asm,bash,php}
 chown "$user_name:$user_name" "$programming_dir"
 mkdir -p $progtools_dir || echo "$progtools_dir is already exist" >&2
 chown "$user_name:$user_name" "$progtools_dir"
-echo -e "\nexport PATH=$PATH:$progtools_dir" >> "$user_home/.$(basename `echo $SHELL`)rc"
+echo -e "\nexport PATH=\$PATH:$progtools_dir" >> "$user_home/.$(basename `echo $SHELL`)rc"
 chown "$user_name:$user_name" $programming_dir/{C,C++,python,asm,bash,php} || exit $?
 
 set_cee_content
